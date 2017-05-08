fcecd8ddac7822c90252f232a4eed24c3d72f9ae
YuriiSalimov <yuriy.alex.salimov@gmail.com>
2017-05-08 14:28:03 +0300

    Code refactoring.

--- js/hash.js
+++ js/hash.js
@@ -1,2 +1,1 @@
-var hash = window.location.hash;
-$(document).scrollTop($('.' + hash.split('#')[1]).offset().top - $('.menu-fix').height());
+var hash = window.location.hash;$(document).scrollTop($('.' + hash.split('#')[1]).offset().top - $('.menu-fix').height());
